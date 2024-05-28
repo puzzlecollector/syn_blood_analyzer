b6b9e1687ab2385b756a40b422631b45e3046d13
kimkevin2657 <kimkevin2657@naver.com>
2024-05-28 21:17:04 +0900

    modified deprecationg

--- common/models.py
+++ common/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django.contrib.postgres.fields import JSONField 
+from django.db.models import JSONField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
